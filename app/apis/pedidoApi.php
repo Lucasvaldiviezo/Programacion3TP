33643a8f289b9f4228bda04c475462e6af76501a
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2021-06-06 18:15:41 -0300

    casi listo el agregado de productos

--- app/apis/pedidoApi.php
+++ app/apis/pedidoApi.php
@@ -8,8 +8,12 @@ require_once "./apis/productoApi.php";
 require_once "./apis/mesaApi.php";*/
 
 use \App\Models\Pedido as Pedido;
+use \App\Models\Empleado as Empleado;
+use \App\Models\Cliente as Cliente;
+use \App\Models\Mesa as Mesa;
+use \App\Models\Producto as Producto;
 
-class PedidoApi extends Pedido implements IApiUsable
+class PedidoApi implements IApiUsable
 {
     public function TraerUno($request, $response, $args) {
         $ped=$args['id'];
@@ -33,10 +37,13 @@ class PedidoApi extends Pedido implements IApiUsable
         $parametros = $request->getParsedBody();
         $cadena = '0123456789abcdefghijklmnopqrstuvwxyz';
         $codigo = substr(str_shuffle($cadena),0,5);
-        $idCliente == $parametros['id_cliente'];
-        $idMesa == $parametros['id_mesa'];
-        $idEmpleado == $parametros['id_empleado'];
-        $estado == 'en preparacion';
+        $idCliente = $parametros['idCliente'];
+        $idMesa = $parametros['idMesa'];
+        $idEmpleado = $parametros['idEmpleado'];
+        $estado = 'en preparacion';
+        $productos = json_decode($parametros['json_productos']);
+        //$listaProductos = json_decode($productos);
+        //var_dump($productos->Productos);
         if($parametros['puesto'] == 'cocina' || $parametros['puesto'] == 'bar' || $parametros['puesto'] == 'candybar')
         {
             $puesto = $parametros['puesto'];
@@ -44,23 +51,45 @@ class PedidoApi extends Pedido implements IApiUsable
         {
             $puesto = 'cocina';
         }
+        
+        //Checkeo
         $empleado = Empleado::where('id', '=', $idEmpleado)->first();
         $cliente = Cliente::where('id', '=', $idCliente)->first();
-        $mesa = Cliente::where('id', '=', $idMesa)->first();
+        $mesa = Mesa::where('id', '=', $idMesa)->first();
         if($empleado != null && $cliente != null && $mesa != null)
         {
-            // Creamos el pedido
-            $ped = new Pedido();
-            $ped->codigo = $codigo;
-            $ped->id_cliente = $idCliente;
-            $ped->id_mesa = $idMesa;
-            $ped->id_empleado = $idEmpleado;
-            $ped->estado = $estado;
-            $ped->puesto = $puesto;
-            $ped->fecha_hora_creacion = date("y-m-d / H:i:s")
-            $ped->ultima_modificacion = date("H:i:s")
-            $ped->save();
-            $payload = json_encode(array("mensaje" => "Pedido creado con exito"));
+            $total = 0;
+            $checkeoProducto = true;
+            foreach($productos->Productos as $prod)
+            {
+                $auxProd = Producto::where('id', $prod->id)->first();
+                if($auxProd == null)
+                {
+                    $checkeoProducto = false;
+                    break;
+                }
+                $total += $auxProd->precio * $prod->cantidad;
+            }
+            if($checkeoProducto)
+            {
+                // Creamos el pedido
+                $ped = new Pedido();
+                $ped->codigo = $codigo;
+                $ped->id_cliente = $idCliente;
+                $ped->id_mesa = $idMesa;
+                $ped->id_empleado = $idEmpleado;
+                $ped->estado = $estado;
+                $ped->puesto = $puesto;
+                $ped->fecha_hora_creacion = date("y-m-d H:i:s");
+                $ped->ultima_modificacion = date("H:i:s");
+                $ped->total = $total;
+                $ped->save();
+                $payload = json_encode(array("mensaje" => "Pedido creado con exito"));
+            }else
+            {
+                $payload = json_encode(array("mensaje" => "No hay stock o no existe el producto"));
+            }
+            
         }else
         {
             $payload = json_encode(array("mensaje" => "Datos erroneos"));
