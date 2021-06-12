6356bca1cc6a5b8e5abcc9d6af958ad669289280
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2021-06-12 04:05:34 -0300

    descarga de PDF lista

--- app/apis/manejoArchivos.php
+++ app/apis/manejoArchivos.php
@@ -30,52 +30,94 @@ class ManejoArchivos
     public function GuardarPDF($request,$response,$next,$tipo)
     {
         $bool = false;
+        $pdf = new FPDF('P','mm','A4');
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',12);
+        $pdf->Cell(190,15,ucfirst($tipo).'s',"0","1","C");
         switch($tipo)
         {
             case 'empleado':
                 $lista = Empleado::all();
-                $empleados = json_encode(array("listaCompleta" => $lista));
-                $datos = $this->DatosToPDF($empleados,$tipo);
+                $empleados = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($empleados->listaCompleta as $emp)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($emp,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,4,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
+                
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de empleados"));
             break;
             case 'cliente':
                 $lista = Cliente::all();
-                $clientes = json_encode(array("listaCompleta" => $lista));
-                $datos =  $this->DatosToPDF($clientes,$tipo);
+                $clientes = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($clientes->listaCompleta as $cli)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($cli,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,4,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de clientes"));
             break;
             case 'mesa':
                 $lista = Mesa::all();
-                $mesas = json_encode(array("listaCompleta" => $lista));
-                $datos = $this->DatosToPDF($mesas,$tipo);
+                $mesas = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($mesas->listaCompleta as $mes)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($mes,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,4,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de mesas"));
             break;
             case 'producto':
                 $lista = Producto::all();
-                $productos = json_encode(array("listaCompleta" => $lista));
-                $datos = $this->DatosToPDF($productos,$tipo);
+                $productos = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($productos->listaCompleta as $prod)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($prod,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,6,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de productos"));
             break;
             case 'pedido':
                 $lista = Pedido::all();
-                $pedidos = json_encode(array("listaCompleta" => $lista));
-                $datos = $this->DatosToPDF($pedidos,$tipo);
+                $pedidos = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($pedidos->listaCompleta as $ped)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($ped,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,4,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de pedidos"));
             break;
             default:
                 $lista = Changelog::all();
-                $changeLogs = json_encode(array("listaCompleta" => $lista));
-                $datos = $this->DatosToPDF($changeLogs,$tipo);
+                $changeLogs = json_decode(json_encode(array("listaCompleta" => $lista)));
+                foreach($changeLogs->listaCompleta as $change)
+                {
+                    $pdf->SetFont('Arial','',10);
+                    $datos = $this->DatosToPDF($change,$tipo);
+                    $datos = iconv('UTF-8', 'windows-1252', $datos);
+                    $pdf->MultiCell(0,4,$datos,"0","L");
+                    $pdf->Ln(1);
+                }
                 $payload = json_encode(array("mensaje" => "Se guardo el PDF de logs"));
         }
         if($datos != null)
         {
-            $pdf = new FPDF('P','mm','A4');
-            $pdf->AddPage();
-            $pdf->SetFont('Arial','B',16);
-            $pdf->Cell(200,20,$tipo.'s',"0","1","C");
-            $pdf->Cell(40,10,$datos,"0","1","C");
-            $pdf->Output('F',$tipo.'.pdf',true);
+            $pdf->Output('F',"./archivos/" . $tipo.'.pdf',true);
             $bool = true;
         }else
         {
@@ -145,46 +187,27 @@ class ManejoArchivos
 
     public function DatosToPDF($datos,$tipo)
     {
-        $lista = json_decode($datos);
         $cadena = "";
         switch($tipo)
         {
             case 'empleado':
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .= "-ID:" . $dato->id . " -Nombre:" . $dato->nombre . " -Apellido" . $dato->apellido . " -Mail" . $dato->mail . " -Clave" . $dato->clave . " -Puesto" . $dato->puesto . "\n";
-                }
+                $cadena .= "- ID: " . $datos->id . ", Nombre: " . $datos->nombre . ", Apellido: " . $datos->apellido . ", Mail: " . $datos->mail . ", Clave: " . $datos->clave . ", Puesto: " . $datos->puesto;
             break;
             case 'cliente':
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .= "{" . $dato->id . "," . $dato->nombre . "," . $dato->apellido . "," . $dato->mail . "," . $dato->dni . "}" .",\n";
-                }
+                    $cadena .= "- ID: " . $datos->id . ", Nombre: " . $datos->nombre . ", Apellido: " . $datos->apellido . ", Mail: " . $datos->mail . ", DNI: " . $datos->dni;
             break;
             case 'mesa':
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .= "{" . $dato->id . "," . $dato->numero . "," . $dato->estado . "}" . ",\n";
-                }
+                    $cadena .= "- ID: " . $datos->id . ", Numero: " . $datos->numero . ", Estado: " . $datos->estado;
             break;
             case 'producto':
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .=  "{" . $dato->id . "," . $dato->nombre . "," . $dato->precio . "," . $dato->stock . "," . $dato->tipo . "}" . ",\n";
-                }
+                    $cadena .=  "- ID: " . $datos->id . ", Nombre: " . $datos->nombre . ", Precio: " . $datos->precio . "," . $datos->stock . ", Stock: " . $datos->tipo;
             break;
             case 'pedido':
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .= "{" . $dato->id . "," . $dato->codigo . "," . $dato->id_cliente . "," . $dato->id_mesa . "," . $dato->datos_productos . "," . $dato->id_empleado . "," .$dato->estado . "," . $dato->total . ",";
-                    $cadena .= $dato->puesto . "," . $dato->fecha_hora_creacion . "," . $dato->ultima_modificacion . "}" . ",\n";
-                }
+                    $cadena .= "- ID: " . $datos->id . ", Codigo: " . $datos->codigo . ", ID Cliente: " . $datos->id_cliente . ", ID Mesa: " . $datos->id_mesa . ", Datos Productos: " . $datos->datos_productos . ", ID Empleado: " . $datos->id_empleado . ", Estado: " .$datos->estado . ", Total: " . $datos->total . ", Puesto: ";
+                    $cadena .= $datos->puesto . ", Fecha y Hora: " . $datos->fecha_hora_creacion . ", Ultima Modificacion:" . $datos->ultima_modificacion;
             break;
             default:
-                foreach($lista->listaCompleta as $dato)
-                {
-                    $cadena .= "{" . $dato->id . "," . $dato->tabla_afectada . "," . $dato->id_afectado . "," . $dato->id_empleado . "," . $dato->accion . "," . $dato->descripcion . "," . $dato->fecha_hora . "}" . ",\n";
-                }
+                    $cadena .= "- ID: " . $datos->id . ", Tabla Afectada: " . $datos->tabla_afectada . ", ID Afectado: " . $datos->id_afectado . ", ID Empleado: " . $datos->id_empleado . ", Accion: " . $datos->accion . ", Descripcion: " . $datos->descripcion . ", Fecha y Hora: " . $datos->fecha_hora;
         }
 
         return $cadena;  
