a233684abe6dd940d68975c59cfa04a7dd278543
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2021-06-13 23:26:47 -0300

    listo

--- app/apis/informacion.php
+++ app/apis/informacion.php
@@ -206,8 +206,6 @@ class Informacion
                     echo "Este año $fechaAño no consumio ningun cliente";
                 }
             break;
-            case 'productos':
-            break;
         }
     }
 
